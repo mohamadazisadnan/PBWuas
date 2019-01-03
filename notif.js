61f39058b0499147e2ca0f24a1898b34be8bcf96
mohamadazisadnan <46299974+mohamadazisadnan@users.noreply.github.com>
2019-01-03 23:28:09 +0700

    Add files via upload

---
+++ notif.js
@@ -0,0 +1,9 @@
+var konfirmasi = confirm("Halaman ini adalah web buatan mahasiswa UDINUS apakah ingin melanjutkan?")
+if(konfirmasi==true)
+{
+  document.location.href="#";
+}
+else
+{
+  document.location.href="#";
+}
